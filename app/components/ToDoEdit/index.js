6e2525243b478bfdc60a4184a2500f1f7b1674a6
Vasanth kumar <vasanthca1997@gmail.com>
2018-01-12 15:09:55 +0530

    time

--- app/components/ToDoEdit/index.js
+++ app/components/ToDoEdit/index.js
@@ -1,68 +1,131 @@
-import styles from './styles'
-import React from 'react';
-import { Text, TouchableOpacity, View ,} from 'react-native';
-import DateTimePicker from 'react-native-modal-datetime-picker';
+// import styles from './styles'
+// import React from 'react';
+// import { Text, TouchableOpacity, View ,} from 'react-native';
+// import DateTimePicker from 'react-native-modal-datetime-picker';
+//
+//
+//
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  PanResponder
+} from 'react-native';
+import DatePicker from './datepicker.js';
 
-  class ToDoEdit extends React.Component {
+class ToDoEdit extends Component {
 
-    state = {
-    isDateTimePickerVisible: false,
-    // isTimePickerVisible : false,
-    selectedDate:null,
-  };
+  constructor(props) {
+    super(props);
 
-  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+    this.state = {
+      date: '',
+      time: '20:00',
+      datetime: '2016-05-05 20:00',
+      datetime1: '2016-05-05 20:00'
+    };
+  }
 
-  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
-
-  // _showTimePicker = () => this.setState ({isTimePickerVisible:true});
-  // _hideTimePicker = () => this.setState ({isTimePickerVisible:false});
-
-
-  _handleDatePicked = (date) => {
-    this.setState({ selectedDate: date });
-    this._hideDateTimePicker();
-  };
-  // _handleTimePicked = (time) => {
-  //   this.setState({selectedTime : time});
-  //   this._hideTimePicker();
+  // componentWillMount() {
+  //   this._panResponder = PanResponder.create({
+  //     onStartShouldSetPanResponder: (e) => {console.log('onStartShouldSetPanResponder'); return true;},
+  //     onMoveShouldSetPanResponder: (e) => {console.log('onMoveShouldSetPanResponder'); return true;},
+  //     onPanResponderGrant: (e) => console.log('onPanResponderGrant'),
+  //     onPanResponderMove: (e) => console.log('onPanResponderMove'),
+  //     onPanResponderRelease: (e) => console.log('onPanResponderRelease'),
+  //     onPanResponderTerminate: (e) => console.log('onPanResponderTerminate')
+  //   });
   // }
 
-  render () {
-    selectedDate = this.state.selectedDate;
-    // selectedTime=this.state.selectedTime;
-    date_ = selectedDate!=null ? selectedDate.toString() : 'select date';
-    // time_= selectedTime!=null ? selectedTime.toString() : 'select date';
+  render() {
     return (
-      <View style={{ flex: 1 }}>
-        <TouchableOpacity onPress={this._showDateTimePicker}>
-          <Text>{date_}</Text>
-        </TouchableOpacity>
-        <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
-          onConfirm={this._handleDatePicked}
-          onCancel={this._hideDateTimePicker}
+      <View >
+        <Text >
+          Welcome to react-native-datepicker example!
+        </Text>
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.date}
+          mode="date"
+          placeholder="placeholder"
+          format="YYYY-MM-DD"
+          minDate="2016-05-01"
+          maxDate="2016-06-01"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          iconSource={require('./google_calendar.png')}
+          onDateChange={(date) => {this.setState({date: date});}}
         />
-        </View>
+        <Text >date: {this.state.date}</Text>
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.time}
+          mode="time"
+          format="HH:mm"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          minuteInterval={10}
+          onDateChange={(time) => {this.setState({time: time});}}
+        />
+        <Text >time: {this.state.time}</Text>
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.datetime}
+          mode="datetime"
+          format="YYYY-MM-DD HH:mm"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          showIcon={false}
+          onDateChange={(datetime) => {this.setState({datetime: datetime});}}
+        />
+        <Text >datetime: {this.state.datetime}</Text>
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.datetime1}
+          mode="datetime"
+          format="YYYY-MM-DD HH:mm"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }
+          }}
+          minuteInterval={10}
+          onDateChange={(datetime) => {this.setState({datetime1: datetime});}}
+        />
+        <Text >datetime: {this.state.datetime1}</Text>
+      </View>
+    );
+  }
+}
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF'
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5
+//   }
+// });
 
-      // <View >
-      // <TouchableOpacity onPress={this._showTimePicker}>
-      //   <Text>{time_}</Text>
-      // </TouchableOpacity>
-      //
-      //    <TimePicker
-      //    selectedHour={0}
-      //    selectedMinute={30}
-      //    minuteInterval={5}
-      //    isVisible={this.state.isTimePickerVisible}
-      //    onConfirm={this._handleTimePicked}
-      //    onCancel={this._hideTimePicker} />
-      //  </View>
-
-     );
-   }
- };
-
-
-
+// AppRegistry.registerComponent('datepicker', () => datepicker);
   module.exports = ToDoEdit;
